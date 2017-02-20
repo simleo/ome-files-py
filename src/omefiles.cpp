c4eb3280454481504aee5a1934dcbd0c7e1b15d8
simleo <simleo@crs4.it>
2017-02-20 17:45:09 +0000

    fixed set_id

--- src/omefiles.cpp
+++ src/omefiles.cpp
@@ -3,8 +3,15 @@
 
 using namespace boost::python;
 
+struct OMETIFFReaderWrapper : ome::files::in::OMETIFFReader, wrapper<ome::files::in::OMETIFFReader> {
+  void setIdStr(std::string filename) {
+    setId(filename);
+  }
+};
+
 BOOST_PYTHON_MODULE(ome_files) {
-  class_<ome::files::in::OMETIFFReader, boost::noncopyable>("OMETIFFReader")
-    .def("set_id", &ome::files::in::OMETIFFReader::setId)
-    .def("get_image_count", &ome::files::in::OMETIFFReader::getImageCount);
+  class_<OMETIFFReaderWrapper, boost::noncopyable>("OMETIFFReader")
+    .def("set_id", &OMETIFFReaderWrapper::setIdStr)
+    .def("get_image_count", &ome::files::in::OMETIFFReader::getImageCount)
+    .def("close", &ome::files::in::OMETIFFReader::close);
 }
