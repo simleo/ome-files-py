89e1245450c5480f82da92c9b4cde66a2f57bccc
simleo <simleo@crs4.it>
2017-04-21 14:53:53 +0100

    examples/movie.py: added objects animation

--- examples/movie.py
+++ examples/movie.py
@@ -3,23 +3,43 @@
 
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import datapackage
+import pandas as pd
 
 import ome_files
+
+
+DP_FN = "trackmate_dp/datapackage.json"
+SCALE = 3.10559006211
+
+
+def get_table(datapackage_fn, table_name):
+    dp = datapackage.DataPackage(datapackage_fn)
+    res_map = dict((_.descriptor["name"], _.local_data_path)
+                   for _ in dp.resources)
+    return pd.read_csv(res_map[table_name])
+
+
+df = get_table(DP_FN, "objects_table")
 
 
 fig = plt.figure()
 
 # ims is a list of lists, each row is a list of artists to draw in the
-# current frame; here we are just animating one artist, the image, in
-# each frame
+# current frame
 ims = []
 reader = ome_files.OMETIFFReader()
 reader.set_id("9I5TT808_F00000010.ome.tif")
 #for i in xrange(reader.get_image_count()):
-for i in xrange(5):
+for i in xrange(20):
     pixels = reader.open_array(i)
     im = plt.imshow(pixels, cmap="gray", animated=True)
-    ims.append([im])
+    df_slice = df[df["FRAME"] == i]
+    x = SCALE * df_slice["POSITION_X"].values
+    y = SCALE * df_slice["POSITION_Y"].values
+    scatter = plt.scatter(x=x, y=y, s=100, edgecolors='b', facecolors='none',
+                          animated=True)
+    ims.append([im, scatter])
 
 ani = animation.ArtistAnimation(fig, ims, interval=500, repeat_delay=1000)
 
